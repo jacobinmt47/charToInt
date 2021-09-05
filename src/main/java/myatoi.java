60b18b9105d7dd4110688bf8c1537b5151a09d3d
jacob l <jacobinmt47@outlook.com>
2021-09-05 15:20:32 -0600

    fails with words

--- src/main/java/myatoi.java
+++ src/main/java/myatoi.java
@@ -4,20 +4,58 @@
  * @author jacob
  */
 public class myatoi {
-    private int sum(String s){
-        s=s.trim();
-        return Integer.parseInt(s);//cheating needs to be done with chars and ints
+
+    private String rmLeadingZeros(String s) {
+        char arr[] = new char[s.length()];
+        int nextChar = 0;
+        Boolean leading = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '0' || leading) {
+                arr[nextChar] = s.charAt(i);
+                nextChar++;
+            } else {
+                leading = false;
+                break;
+            }
+        }
+        String ret = new String(arr);
+        return ret.strip();
     }
+
+    private int sum(String s) {
+        s = s.trim();
+        int len = s.length() - 1;
+        int powten = 1;
+        int arr[] = new int[10];
+        for (int i = 0; i < s.length(); i++) {
+            int l = (len - i);
+            arr[i] = s.charAt(l) - '0';
+            arr[i] = arr[i] * powten;
+            powten = powten * 10;
+
+        }
+        int sum = 0;
+        for (int j = 0; j < s.length(); j++) {
+            sum = sum + arr[j];
+        }
+        return sum;
+    }
+
     private String onlyDigits(String s) {
         char array[] = new char[s.length()];
         int nextchar = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
+            if ((s.charAt(i)>='0'&&(s.charAt(i)<='9'))) {
                 array[nextchar] = s.charAt(i);
                 nextchar++;
             }
+            else{
+                return "0";
+            }
         }
-        return new String(array);
+        String ret = new String(array);
+
+        return ret.trim();
     }
 
     private Boolean isMax(String s, Boolean isPositive) {
@@ -30,8 +68,20 @@ public class myatoi {
             }
             if (s.charAt(0) == '2') {
                 //do lots of stuff based on if greater that 2**31-1 or less that 2**31
+                char arr[] = new char[9];
+                for (int i = 0; i < 9; i++) {
+                    arr[i] = s.charAt(i + 1);
+                }
+                int sht = this.sum(new String(arr));
+                if (sht > (147483647) && isPositive) {
+                    return true;
+                }
+                if (sht > (147483648) && !isPositive) {
+                    return true;
+                }
+                return false;
             }
-            return true;
+            return true; // 3 or greater
         }
         //string should be under 10 digits
         return false;
@@ -44,48 +94,47 @@ public class myatoi {
      * @return
      */
     private Boolean isGoodChar(Character c) {
-        Boolean d = Character.isDigit(c);
+        Boolean d = (c>='0'&&c<='9');
         Boolean p = (c == '+');
         Boolean m = (c == '-');
         Boolean r = (c == '.');
         return (d || p || m || r);
     }
 
-    private String splitPeriod(String s) {
-        return s.split("/[^.]*/")[0];//find first string that has a period
-    }
-
     public int myAtoi(String s) {
         Boolean isPositive = true;
         s = s.toLowerCase();
         s = s.strip();
-        if (s.isBlank()) {
-            return 0;
-        }
-        if (!this.isGoodChar(s.charAt(0))) {
+        if (s.contains("+-") || s.contains("-+")) {
             return 0;
         }
         if (s.indexOf('.') > -1) {
-            s = this.splitPeriod(s);
+            String t = s.substring(0, s.indexOf('.'));
+            s = t;
         }
-        //TODO:find if negative remove any tailing words
+        if (s.isBlank()) {
+            return 0;
+        }
+
         if (s.charAt(0) == '-') {
             isPositive = false;
         }
-        s = this.onlyDigits(s);
-        if(this.isMax(s, isPositive)){
-            if(isPositive){
-                return Integer.MAX_VALUE;
+        if (!this.isGoodChar(s.charAt(0))) {
+                return 0;
             }
-            else{
+        s = this.onlyDigits(s);
+        s = this.rmLeadingZeros(s);
+        if (this.isMax(s, isPositive)) {
+            if (isPositive) {
+                return Integer.MAX_VALUE;
+            } else {
                 return Integer.MIN_VALUE;
             }
         }
-        if(isPositive){
+        if (isPositive) {
             return this.sum(s);
-        }
-        else{
-            return 0-(this.sum(s));
+        } else {
+            return 0 - (this.sum(s));
         }
     }
 
@@ -95,6 +144,14 @@ public class myatoi {
         System.out.println(i);
         i = m.myAtoi("234 words");
         System.out.println(i);
-    }
+       i = m.myAtoi("00000-42a1234");
+        System.out.println(i);
+        System.out.println(m.myAtoi("9999999999"));
+        System.out.println(m.myAtoi("4193 with words"));
+        System.out.println(m.myAtoi("3.14159"));
+        System.out.println(m.myAtoi("21474836460"));
+        System.out.println(m.myAtoi("0100"));
+        System.out.println(m.myAtoi("+-12"));
 
+    }
 }
